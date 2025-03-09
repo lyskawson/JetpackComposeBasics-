2a455780448bb19704e909509ca10436bdde9e60
Aleksander Łyskawa <alek.lyskawa@gmail.com>
2025-03-09 19:33:57 +0100

    Add floatinButton example

--- app/src/main/java/com/example/layoutcomposables/ScaffoldExample.kt
+++ app/src/main/java/com/example/layoutcomposables/ScaffoldExample.kt
@@ -1,11 +1,54 @@
 package com.example.layoutcomposables
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaffoldExample(modifier: Modifier = Modifier) {
+fun ScaffoldExample(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title =  { Text("My Home Page") },
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}){
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+
+    ){
+        Column(
+            modifier = modifier.padding(it).fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ){}
+
+    }
     
 }
 
@@ -13,6 +56,8 @@ fun ScaffoldExample(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun ScaffoldExamplePreview() {
+    ScaffoldExample()
+
 
     
 }
